eb7694ce488ee1f2cbb870ac0e59b97735eb1c0a
Bryan Chen <xlchen1291@gmail.com>
2016-10-07 15:03:01 +1300

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "extends": "standard",
+    "installedESLint": true,
+    "plugins": [
+        "standard",
+        "promise"
+    ],
+    "env": {
+        "node": true,
+        "es6": true,
+    }
+};
