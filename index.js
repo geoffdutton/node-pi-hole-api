bad50ac341be9d0392dbafebeb28ee23e678c364
Bryan Chen <xlchen1291@gmail.com>
2016-10-07 15:12:19 +1300

    basic express setup

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 require('dotenv').config()
+
+const express = require('express')
+const compression = require('compression')
+const logger = require('morgan')
+
+const app = express()
+
+app.set('port', process.env.PORT || 3000)
+app.use(compression())
+app.use(logger('dev'))
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'))
+})
+
+module.exports = app
